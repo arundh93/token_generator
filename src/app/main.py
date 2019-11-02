1eee68630bc417ba29453c5812e4b5208619eeff
Arun Dhingra <arundh@Aruns-MacBook-Pro.local>
2019-11-02 14:03:22 +0530

    Adds functionality for generating tokens

--- src/app/main.py
+++ src/app/main.py
@@ -1,11 +1,15 @@
-from flask import Flask
+import os
+
+from flask import Flask, request, jsonify
+import hashlib
 
 app = Flask(__name__)
 
-
-@app.route("/")
-def hello():
-    return "Hello World!"
+os.environ['PEPPER'] = '12345678'
+@app.route("/", methods=['POST'])
+def tokenize():
+    pepper = os.environ['PEPPER']
+    return jsonify(list(map(lambda x: hashlib.sha3_256((x + pepper).encode()).hexdigest(), request.json['data'])))
 
 
 if __name__ == '__main__':
